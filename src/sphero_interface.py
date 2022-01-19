7b851ecd7f6294080fb3ff6f7d387c3fb0a7d045
jstaley <james.staley625703@tufts.edu>
2022-01-18 19:23:06 -0500

    remove velocity read. messes up sensor read for some reason.

--- src/sphero_interface.py
+++ src/sphero_interface.py
@@ -13,12 +13,12 @@ from std_msgs.msg import Float32
 from geometry_msgs.msg import Pose2D, PoseStamped # hijack the Pose2D message. x = timestep. theta = heading. y = velocity.
 import rospy
 
-ACTIVE_SENSORS = [CoreTime, Velocity]
+ACTIVE_SENSORS = [CoreTime, Quaternion]
 heartbeat_period = 0
 # Sphero set
 spheros = {
     # "d9:81:9e:b8:ad": None,
-    "f0:35:04:88:07:76": None,
+    # "f0:35:04:88:07:76": None,
     "c9:b4:ef:32:eC:28": None,
 }
 
@@ -38,15 +38,23 @@ class WrappedSphero(Sphero):
         self.light_pub = rospy.Publisher(self.name+"/light", Float32, queue_size=1)
         self.velocity_pub = rospy.Publisher(self.name+"/velocity", Float32, queue_size=1)
         
+        self.sensors_setup = False
+        self.adaptor_setup = False
         self.setup()
 
     def setup(self):
         try:
             self.ble_adapter = self._ble_adapter_cls(self.mac_address)
-            rospy.sleep(0.1)
-            self.sensor.set_notify(self.sensor_bt_cb, *ACTIVE_SENSORS) #) #
+            self.adaptor_setup = True
         except:
-            print("WARN: Could not connect to "+self.name)
+            print("WARN: Could not connect to bluetooth adapter for "+self.name)
+    
+    def init_sensor_read(self):
+        try:
+            self.sensor.set_notify(self.sensor_bt_cb, *ACTIVE_SENSORS) #) #
+            self.sensors_setup = True
+        except:
+            print("WARN: Could not connect to sensors for "+self.name)
 
     def cmd_cb(self, pose2d):
         self.vector = pose2d
@@ -56,26 +64,34 @@ class WrappedSphero(Sphero):
         self.vector.theta = min(360, max(self.vector.theta, 0)) # todo: correct for wrap
 
     def sensor_bt_cb(self, data:dict):
+        # info = ", ".join("{:1.2f}".format(data.get(param)) for param in Quaternion)
+        # print(f"[{data.get(CoreTime.core_time):1.2f}] Quaternion (x, y, z, w): {info}")
+        # print("=" * 60)
         sensed_vector = PoseStamped()
         # Send along data from sphero to ros
         sensed_vector.header.stamp = data.get(CoreTime.core_time)
-        sensed_vector.pose.position.x = data.get(Velocity.x)
-        sensed_vector.pose.position.y = data.get(Velocity.y)
-        sensed_vector.pose.position.z = 0
+        # sensed_vector.pose.position.x = data.get(Velocity.x)
+        # sensed_vector.pose.position.y = data.get(Velocity.y)
+        # sensed_vector.pose.position.z = 0
         sensed_vector.pose.orientation.x = data.get(Quaternion.x)
         sensed_vector.pose.orientation.y = data.get(Quaternion.y)
         sensed_vector.pose.orientation.z = data.get(Quaternion.z)
         sensed_vector.pose.orientation.w = data.get(Quaternion.w)
-        self.velocity_pub.publish(sensed_vector)
+
+        print(sensed_vector)
+        # self.velocity_pub.publish(sensed_vector)
 
     def step(self):
+        if not self.sensors_setup: self.init_sensor_read()
+
+
         # cap inputs
         t, v, theta = self.vector.x, self.vector.y, self.vector.theta
         if (time() - t > 3.0 and v > 0):
             print("forcing 0 velocity due to stale command.")
             self.vector = Pose2D()
+        # print(self.name+"(v,theta) : {} {}".format(v, theta))
 
-        print(self.name+"(v,theta) : {} {}".format(v, theta))
         self.light_pub.publish(self.sensor.get_ambient_light_sensor_value())
 
         self.driving.drive_with_heading(int(v), int(theta), Direction.forward)
