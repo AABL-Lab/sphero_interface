e7ecd4550f9afb9fa2702249baf421dd95e3da8c
j <dirkmcpherson@gmail.com>
2022-05-09 15:11:39 -0400

    add IZ to config. Don't allow non-priority goals in the interaction zone

--- src/goal_generator.py
+++ src/goal_generator.py
@@ -16,12 +16,11 @@ from geometry_msgs.msg import Pose2D
 from sphero_interface.msg import SpheroNames, PositionGoal
 from geometry_msgs.msg import Pose2D
 from copy import deepcopy
+import numpy as np
 
 w_range = (rospy.get_param("/tracker_params/min_width", 150.), rospy.get_param("/tracker_params/max_width", 800.))
 h_range = (rospy.get_param("/tracker_params/min_height", 75.), rospy.get_param("/tracker_params/max_height", 450))
 GOAL_THRESHOLD = 3 * rospy.get_param("/param_server/expected_sphero_radius", default=30) # How far we can be from a goal before its considered achieved
-
-
 PRESET_GOALS = {} #{"sd1": [(350, 200), (750, 200)]}
 PRESET_GOALS_IDX = {"sd1": 0}
 
@@ -30,8 +29,31 @@ current_poses = dict()
 goal_publishers = dict()
 pose_subscribers = dict()
 
-def generate_random_goal():
-    return Pose2D(random.randint(*w_range), random.randint(*h_range), 0)
+INTERACTION_ZONE = (rospy.get_param("/param_server/x_interaction_zone", default=470), rospy.get_param("/param_server/y_interaction_zone", default=80))
+PRECOMPUTE_GOALS = True
+precomputed_goals = []
+if PRECOMPUTE_GOALS:
+    MIN_DISTANCE_TO_IZ = rospy.get_param("/param_server/min_distance_to_iz", default=80)
+    xres, yres = 40, 40 # x and y resolutions of random goals, the total number of goals will be < xres * yres because we're not using an goals within a radius of the interaction zone 
+    for x in np.linspace(w_range[0], w_range[1], xres):
+        for y in np.linspace(h_range[0], h_range[1], yres):
+            r = np.sqrt((x-INTERACTION_ZONE[0])**2 + (y-INTERACTION_ZONE[1])**2)
+            if (r >= MIN_DISTANCE_TO_IZ):
+                precomputed_goals.append(Pose2D(x, y, 0))
+
+# Show the valid goals
+import matplotlib.pyplot as plt
+plt.scatter([pt.x for pt in precomputed_goals], [pt.y for pt in precomputed_goals])
+plt.show()
+
+
+def select_random_goal():
+    goal = None
+    if PRECOMPUTE_GOALS:
+        goal = random.choice(precomputed_goals)
+    else:
+        goal = Pose2D(random.randint(*w_range), random.randint(*h_range), 0)
+    return goal 
 
 def main():
     rospy.init_node("goal_generator")
@@ -48,10 +70,10 @@ def main():
                     PRESET_GOALS_IDX[k] += 1
                     new_goal = PositionGoal(k, Pose2D(p_goals[p_idx][0], p_goals[p_idx][1], 0))
                 else:
-                    rgoal = generate_random_goal()
+                    rgoal = select_random_goal()
                     idiot_idx = 0
                     while any([(utils.pose2d_distance(rgoal, g.goal) < 100) for g in goals]):
-                        rgoal = generate_random_goal()
+                        rgoal = select_random_goal()
                         idiot_idx += 1
                         if idiot_idx > 100: rospy.logwarn(f"you wrote this wrong...")
 
